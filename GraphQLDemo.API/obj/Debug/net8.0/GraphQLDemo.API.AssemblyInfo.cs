31d97336b52a338c52ba9d75bc25586780742ded
Tayyab94 <tayyab.bhatti30@gmail.com>
2024-07-17 19:10:03 +0500

    de

--- GraphQLDemo.API/obj/Debug/net8.0/GraphQLDemo.API.AssemblyInfo.cs
+++ GraphQLDemo.API/obj/Debug/net8.0/GraphQLDemo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQLDemo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39570add4d2a8e2d82468a351fb4c6fa1ed1117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c8d576608de46764047bf268c5ebbddb551459")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQLDemo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQLDemo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
